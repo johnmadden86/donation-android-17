90e847cb92de1bc16328a2d13ae85464a19a602f
John Madden <johnmadden86@gmail.com>
2017-11-15 11:19:07 +0000

    last donation lab complete

--- app/src/main/java/app/donation/main/DonationApp.java
+++ app/src/main/java/app/donation/main/DonationApp.java
@@ -1,4 +1,4 @@
-package app.donation;
+package app.donation.main;
 
 import android.app.Application;
 import android.util.Log;
@@ -7,11 +7,14 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import app.donation.model.Donation;
+import app.donation.model.User;
+
 public class DonationApp extends Application {
 
     public final int target = 10000;
     public int totalDonated = 0;
-    private String totalDonatedString = "Total: " + totalDonated;
+    public String totalDonatedString;
     public List<Donation> donations = new ArrayList<>();
     public List<User> users = new ArrayList<>();
 
@@ -21,19 +24,29 @@ public class DonationApp extends Application {
         Log.v("Donate", "Donation App Started");
     }
 
-    public boolean newDonation(Donation donation) {
+    public void newDonation(Donation donation) {
         boolean targetAchieved = totalDonated > target;
         if (!targetAchieved) {
             donations.add(donation);
             totalDonated += donation.amount;
+            totalDonatedString = "Total: €" + totalDonated;
+            Log.v("Donate", donation.amount + " donated by " + donation.method + "\nCurrent total " + totalDonated);
         } else {
             Toast toast = Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT);
             toast.show();
         }
-        return targetAchieved;
     }
 
     public void newUser (User user) {
         users.add(user);
+    }
+
+    public boolean validUser (String email, String password) {
+        for (User user : users) {
+            if (user.email.equals(email) && user.password.equals(password)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
